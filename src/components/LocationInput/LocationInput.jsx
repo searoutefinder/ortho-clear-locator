02e38fa761ebe0ac11d2180f490f954665c7007b
Tamas Hajdu <info@ezmapdesign.com>
2025-11-08 17:09:40 +0100

    Location input minor fix

--- src/components/LocationInput/LocationInput.jsx
+++ src/components/LocationInput/LocationInput.jsx
@@ -194,21 +194,21 @@ const LocationInput = ({onChange, onArrowDown, onArrowUp, onEnter, suggestions, 
   return (
     <div className="transition-all duration-300 ease-in-out bg-[#3b3b3b] absolute w-full md:left-0 md:top-0 h-20 p-4 z-999">
       <div className="flex gap-x-2 justify-center items-center w-full h-full">
-        <div className="flex bg-[#FFF] w-full h-full rounded-3xl shadow-md pr-0">
+        <div className="flex bg-[#FFF] w-full h-full rounded-3xl shadow-md pr-6">
           <input 
             ref={inputRef}
             value={query} 
             onChange={(e) => setQuery(e.target.value)}
             onKeyDown={keyDownHandler}
             type="text" 
-            className="text-sm w-full flex flex-col focus:outline-none bg-[#FFF] p-3 pl-6 rounded-tl-3xl rounded-bl-3xl" 
+            className="text-sm w-full flex flex-col focus:outline-none bg-[#FFF] p-3 pl-6 pr-6 rounded-tl-3xl rounded-bl-3xl" 
             placeholder="Enter your address"
           />
           {
             isClearButtonVisible  && 
               <button
                 onClick={clearInputHandler} 
-                className="flex relative right-10 top-0 justify-center items-center cursor-pointer"
+                className="flex relative right-4 top-0 justify-center items-center cursor-pointer"
                 title="Clear Input"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#3b3b3b" className="size-6">
@@ -218,7 +218,7 @@ const LocationInput = ({onChange, onArrowDown, onArrowUp, onEnter, suggestions, 
           }
           <button
             onClick={geolocationClickHandler} 
-            className="relative right-6 top-0 flex justify-center items-center cursor-pointer" 
+            className="relative right-[-12] top-0 flex justify-center items-center cursor-pointer" 
             title="Use my current location"
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#3b3b3b" className="size-6">
